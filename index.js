44765cbaa1af8e635d5721f7db938241dd8cb6a2
Nazmul Kabir Rifat <nk.rifat76@gmail.com>
2026-05-22 16:32:45 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// middleware
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
