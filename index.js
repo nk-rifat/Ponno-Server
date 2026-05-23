137e72b10b2f0b86459361b9c7dd16041abeea7c
Nazmul Kabir Rifat <nk.rifat76@gmail.com>
2026-05-23 12:16:47 +0600

    update index.js

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const dotenv = require("dotenv");
-const connectDB = require("./config/db");
+require("dotenv").config();
 const app = require("./app");
+const connectDB = require("./config/db");
 
-// load env variables
-dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 // connect database
 connectDB();
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
