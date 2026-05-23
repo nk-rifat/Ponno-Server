0c5c6cd3d476377fc19ab08109c26fdf4685697b
Nazmul Kabir Rifat <nk.rifat76@gmail.com>
2026-05-23 11:53:43 +0600

    write the business logic for all products

---
+++ controllers/productController.js
@@ -0,0 +1,23 @@
+const Product = require("../models/Product");
+
+// GET ALL PRODUCTS
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+
+    res.status(200).json({
+      success: true,
+      count: products.length,
+      data: products,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch products",
+    });
+  }
+};
+
+module.exports = {
+  getAllProducts,
+};
