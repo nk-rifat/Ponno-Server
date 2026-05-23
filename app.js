f8293c1ad03c48a5219e41750852d9048d782083
Nazmul Kabir Rifat <nk.rifat76@gmail.com>
2026-05-23 12:14:48 +0600

    update the app.js

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const cors = require("cors");
+
+const productRoutes = require("./routes/productRoutes");
+
 const app = express();
 
+// Middlewares
+app.use(cors());
 app.use(express.json());
 
+// Routes
+app.use("/api/products", productRoutes);
+
+// Base Route
 app.get("/", (req, res) => {
-  res.send("Server is running...");
+  res.send("Ponno Server Running...");
 });
 
 module.exports = app;
