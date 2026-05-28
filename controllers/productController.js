83e28ce7d6bc5bb511ece48dda5261e4d9e8a5a2
Nazmul Kabir Rifat <nk.rifat76@gmail.com>
2026-05-28 14:46:55 +0600

    implement pricing query filter

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,12 +3,29 @@ const Product = require("../models/Product");
 // GET ALL PRODUCTS
 const getAllProducts = async (req, res) => {
   try {
-    const { category } = req.query;
+    const { category, price } = req.query;
 
     let filter = {};
 
+    //category filter
     if (category) {
       filter.category = category;
+    }
+
+    //price filter
+    if (price.includes("+")) {
+      const min = price.replace("+", "");
+
+      filter.price = {
+        $gte: Number(min),
+      };
+    } else {
+      const [min, max] = price.split("-");
+
+      filter.price = {
+        $gte: Number(min),
+        $lte: Number(max),
+      };
     }
     const products = await Product.find(filter);
 
