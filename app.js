463e2ebfa398ed3de6178e31d977c38aca1ed426
Nazmul Kabir Rifat <nk.rifat76@gmail.com>
2026-06-01 11:24:49 +0600

    setup cookieparser and cors

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const cookieParser = required("cookie-parser");
 
 const productRoutes = require("./routes/productRoutes");
 
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
+app.use(cookieParser());
 
 // Routes
 app.use("/api/products", productRoutes);
