5c7855e10066ad4b7e3dbc1ab8aae933047df467
Nazmul Kabir Rifat <nk.rifat76@gmail.com>
2026-06-01 15:31:58 +0600

    use routes in app.js file

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const cookieParser = required("cookie-parser");
+const cookieParser = require("cookie-parser");
 
 const productRoutes = require("./routes/productRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(cookieParser());
 
 // Routes
 app.use("/api/products", productRoutes);
+app.use("/api/auth", authRoutes);
 
 // Base Route
 app.get("/", (req, res) => {
