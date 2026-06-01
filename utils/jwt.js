b7ff2b843c865a58430f271dda733c8fb2261270
Nazmul Kabir Rifat <nk.rifat76@gmail.com>
2026-06-01 18:28:06 +0600

    update userSchema and jwt.js file

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,16 +1,18 @@
 const jwt = require("jsonwebtoken");
 
 // Access Token ( short life)
-const createAccessToken = (user) =>
-  jwt.sign(user, process.env.ACCESS_SECRET, {
+const createAccessToken = (payload) => {
+  jwt.sign(payload, process.env.ACCESS_SECRET, {
     expiresIn: "15min",
   });
+};
 
 // Refresh Token (long life)
-const createRefreshToken = (user) =>
-  jwt.sign(user, process.env.REFRESH_SECRET, {
+const createRefreshToken = (payload) => {
+  jwt.sign(payload, process.env.REFRESH_SECRET, {
     expiresIn: "7d",
   });
+};
 
 module.exports = {
   createAccessToken,
