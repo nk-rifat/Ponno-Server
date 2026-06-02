6593836c960e26f0f9c6f5a5e99adf29b8bfde65
Nazmul Kabir Rifat <nk.rifat76@gmail.com>
2026-06-02 11:18:59 +0600

    remove Firebase auth and prepare for JWT-based authentication

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-const cookieParser = require("cookie-parser");
 
 const productRoutes = require("./routes/productRoutes");
-const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 // Middlewares
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors());
 app.use(express.json());
-app.use(cookieParser());
 
 // Routes
 app.use("/api/products", productRoutes);
-app.use("/api/auth", authRoutes);
 
 // Base Route
 app.get("/", (req, res) => {
