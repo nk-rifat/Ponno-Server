5459081a34a069a588e2a9c711a546e8191c9b5c
Nazmul Kabir Rifat <nk.rifat76@gmail.com>
2026-06-10 19:57:21 +0600

    write indexing and export

--- models/Order.js
+++ models/Order.js
@@ -62,3 +62,10 @@ const orderSchema = new mongoose.Schema(
   },
   { timestamps: true },
 );
+
+// Indexing
+orderSchema.index({ userId: 1, createdAt: -1 });
+orderSchema.index({ status: 1, createdAt: -1 });
+
+const Order = mongoose.model("Order", orderSchema);
+module.exports = Order;
