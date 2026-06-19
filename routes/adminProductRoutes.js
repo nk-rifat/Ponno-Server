6c762454125d4a96b87bbce2f9e3ac6fef9116b4
Nazmul Kabir Rifat <nk.rifat76@gmail.com>
2026-06-19 07:20:21 +0600

    add on the routes

--- routes/adminProductRoutes.js
+++ routes/adminProductRoutes.js
@@ -3,6 +3,9 @@ const express = require("express");
 const {
   getAdminProducts,
   deleteProduct,
+  getProductById,
+  updateProduct,
+  createProduct,
 } = require("../controllers/adminProductController");
 
 const router = express.Router();
@@ -10,6 +13,21 @@ const router = express.Router();
 const { verifyAccessToken, verifyAdmin } = require("../middleware/auth");
 
 router.get("/products", verifyAccessToken, verifyAdmin, getAdminProducts);
+router.post(
+  "/products",
+  verifyAccessToken,
+  verifyAdmin,
+  upload.array("images", 3),
+  createProduct,
+);
+router.put(
+  "/products/:id",
+  verifyAccessToken,
+  verifyAdmin,
+  upload.array("images", 3),
+  updateProduct,
+);
 router.delete("/products/:id", verifyAccessToken, verifyAdmin, deleteProduct);
+router.get("/products/:id", verifyAccessToken, verifyAdmin, getProductById);
 
 module.exports = router;
