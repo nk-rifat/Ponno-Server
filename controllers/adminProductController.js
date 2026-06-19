49ef0c7b287a439b8eba111e81a49a4dea9a34ce
Nazmul Kabir Rifat <nk.rifat76@gmail.com>
2026-06-19 06:54:42 +0600

    make the create product api logic

--- controllers/adminProductController.js
+++ controllers/adminProductController.js
@@ -1,5 +1,7 @@
 const Product = require("../models/Product");
+const { cloudinary } = require("../config/cloudinary");
 
+// get products
 exports.getAdminProducts = async (req, res) => {
   try {
     const { search, category, status, page = 1, limit = 9 } = req.query;
@@ -46,5 +48,51 @@ exports.deleteProduct = async (req, res) => {
     const product = await Product.findByIdAndDelete(req.params.id);
   } catch (error) {
     res.status(500).json({ message: "Failed to delete product" });
+  }
+};
+
+// create product
+exports.createProduct = async (req, res) => {
+  try {
+    const images = req.files.map((file) => file.path) || [];
+
+    if (images.length === 0) {
+      return res
+        .status(400)
+        .json({ message: "At least one image is required" });
+    }
+
+    const {
+      productName,
+      description,
+      price,
+      discountPrice,
+      stock,
+      category,
+      subCategory,
+      material,
+      size,
+      color,
+      shape,
+    } = req.body;
+
+    const product = await Product.create({
+      productName,
+      description,
+      price: Number(price),
+      discountPrice: discountPrice ? Number(discountPrice) : null,
+      stock: Number(stock),
+      category,
+      subCategory,
+      material,
+      size,
+      color,
+      shape,
+      images,
+    });
+
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to create product" });
   }
 };
